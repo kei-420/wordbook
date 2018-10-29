e57d1944387edbe4ef968526c0576d090a1bc797
kei-420 <mail.yoshizumi@gmail.com>
2018-10-29 20:17:48 +0900

    Updated

--- wordbook/views.py
+++ wordbook/views.py
@@ -45,24 +45,37 @@ class WordAddView(LoginRequiredMixin, generic.FormView):
 class RepeatedGameView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         global is_game_word, is_answer
+        # 現在ログイン中のユーザー情報から単語データを取得。
         user_word_data = Wordbook.objects.filter(user=request.user)
+        # リストからランダム抽出（"word__vocab"と"word__vocab_meaning"）＆重複なし。
         is_randomly_selected = np.random.choice(
             list(user_word_data.values('word__vocab', 'word__vocab_meaning')),
             5,
             replace=False,
         )
+        # for内では特定の要素の取得
         for j in is_randomly_selected:
+            # 出題される単語
             is_game_word = j['word__vocab']
+            # 答えである意味
             is_answer = j['word__vocab_meaning']
 
+        # ４つの選択肢のうち、答え以外の３つの取得
         choices = Word.objects.exclude(vocab_meaning=is_answer).values('vocab_meaning')
+        # リストからランダム抽出。重複なし。
         random_choices = np.random.choice(list(choices), 3, replace=False)
+        # forを抜けた後用に空のリストを用意。
         list_random_choices = []
+        # for内では残り３つの選択肢の取得。
         for n in range(0, 3):
             are_elements = random_choices[n]['vocab_meaning']
+            # 取得したデータを空リスト内に格納。
             list_random_choices.append(are_elements)
+        # 加えて、答えもその中に格納
         list_random_choices.append(is_answer)
+        # ランダムに選択肢を表示する為にシャッフル。
         random.shuffle(list_random_choices)
+        # choice1 ~ choice4までの定義
         choice1 = list_random_choices[0]
         choice2 = list_random_choices[1]
         choice3 = list_random_choices[2]
