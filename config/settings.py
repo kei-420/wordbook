059ca45716b832f15cd6454a047fa70b34f31f9f
kei-420 <mail.yoshizumi@gmail.com>
2018-11-07 18:53:12 +0900

    Updated

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_NAME = os.path.basename(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -122,7 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+# STATIC_ROOT = '/var/www/{}/static'.format(PROJECT_NAME)
+
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'projectstatic')
 # STATICFILES_FINDERS = (
@@ -130,6 +133,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 #     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 # )
+
+
+
 INSTALLED_APPS += ('debug_toolbar',)
 
 
